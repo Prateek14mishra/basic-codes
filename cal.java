e3c2c2d610a63eb92684fbf1804afc04d73890a9
Prateek14mishra <38374926+Prateek14mishra@users.noreply.github.com>
2018-04-16 00:09:48 +0530

    simple calc with Simple level Inheritance
    Simple calculate for addition, subtraction, multiplication and division with the global variable and local value and the class is called with the process of inheritance
    Simple level Inheritance

---
+++ cal.java
@@ -0,0 +1,11 @@
+class cal extends calc
+{
+	public static void main(String args[])
+	{
+		calc obj=new calc();
+		obj.add();
+		obj.sub();
+		obj.multi();
+		obj.divide();
+	}
+}
---
+++ calc.java
@@ -0,0 +1,32 @@
+ class calc
+{
+	int a,b,r=0;
+	void add()
+	{
+		a=5;
+		b=15;
+		r=a+b;
+	System.out.println(r);
+	}
+	void sub()
+	{
+		a=10;
+		b=5;
+		r=a-b;
+		System.out.println(r);
+	}
+	void multi()
+	{
+		a=5;
+		b=10;
+		r=a*b;
+		System.out.println(r);
+	}
+	void divide()
+	{		
+		a=10;
+		b=5;
+		r=a/b;
+		System.out.println(r);
+	}
+}
